1ee2c0d5075cf8e366453b1190caf63186807c63
Sarah Charles <sarah.charles007@gmail.com>
2017-06-13 09:56:28 -0400

    project started

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
+const morgan = require('morgan');
+
+app.use(morgan(function( tokens, req, res){
+  return [tokens.method(req, res), tokens.url(req, res), tokens.status(req, res)].join(' ');
+}));
 
 app.get('/', function (req, res) {
   res.send('Welcome friend :)');
