e41a4555c5ef6fac03c2b257d3679af6924d0ccd
Sarah Charles <sarah.charles007@gmail.com>
2017-06-13 10:29:48 -0400

    starting templating

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 const morgan = require('morgan');
+const nunjucks = require('nunjucks');
 
 app.use(morgan(function( tokens, req, res){
   return [tokens.method(req, res), tokens.url(req, res), tokens.status(req, res)].join(' ');
 }));
 
+var locals = {
+    title: 'Hello!',
+    people: [ {name: 'Grace'}, {name: 'Sarah'}, {name: 'Hermione'}]
+  };
+
+nunjucks.configure(['views']);
+nunjucks.render('index.html', locals, function (err, output) {
+    if (err) throw err;
+    console.log(output);
+  });
+
 app.get('/', function (req, res) {
-  res.send('Welcome friend :)');
+  res.send('hi!');
 });
 
 
