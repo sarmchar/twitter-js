77880f9ed7c6298806f6af4bdf0c4a2b88257b7e
Grace Gately <gracemgately@gmail.com>
2017-06-13 11:28:13 -0400

    stylesheets

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 const morgan = require('morgan');
-const nunjucks = require('nunjucks');
+//const nunjucks = require('nunjucks');
+const routes = require('./routes');
 
 app.use(morgan(function( tokens, req, res){
   return [tokens.method(req, res), tokens.url(req, res), tokens.status(req, res)].join(' ');
 }));
 
-var locals = {
-    title: 'Hello!',
-    people: [ {name: 'Grace'}, {name: 'Sarah'}, {name: 'Hermione'}]
-  };
+//app.use('/', routes);
 
-app.set('view engine', 'html');
-app.engine('html', nunjucks.render);
-nunjucks.configure('views', {noCache: true});
+// var locals = {
+//     title: 'Hello!',
+//     people: [ {name: 'Grace'}, {name: 'Sarah'}, {name: 'Hermione'}]
+//   };
 
-nunjucks.render('index.html', locals, function (err, output) {
-    if (err) throw err;
-    console.log(output);
-  });
+//  app.set('view engine', 'html');
+//  app.engine('html', nunjucks.render);
+//  nunjucks.configure('views', {noCache: true});
+ 
+
+// nunjucks.render('index.html', locals, function (err, output) {
+//     if (err) throw err;
+//     console.log(output);
+//   });
 
 app.get('/', function (req, res) {
-  res.render('index', locals);
-  //res.send('hi!');
+  //res.render('index', locals);
+  res.send('hi!');
+});
+
+app.get('/stylesheets/style.css', function (req, res) {
+  //res.render('index', locals);
+  res.sendFile(__dirname + '/public/stylesheets/style.css');
 });
 
 
